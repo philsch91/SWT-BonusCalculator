42b6b1d217f04882d4865fb3c2b32df754d14181
philipp <philipp@Philipps-MacBook.local>
2020-02-22 14:35:54 +0100

    initial commit

---
+++ src/test/java/at/fhcampuswien/swtbonuspayment/AppTest.java
@@ -0,0 +1,20 @@
+package at.fhcampuswien.swtbonuspayment;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
